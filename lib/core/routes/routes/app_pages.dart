49a8608b11f2dcac3bcda764b37411e2eeb6b6b6
ShiruRino <tigugg654@gmail.com>
2025-08-27 10:02:38 +0700

    First commit

--- lib/core/routes/routes/app_pages.dart
+++ lib/core/routes/routes/app_pages.dart
@@ -2,11 +2,35 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:pendaftaran_app/core/routes/routes/app_routes.dart';
 import 'package:pendaftaran_app/modules/auth/bindings/auth_binding.dart';
 import 'package:pendaftaran_app/modules/home/views/home_view.dart';
+import 'package:pendaftaran_app/modules/login/views/login_view.dart';
+import 'package:pendaftaran_app/modules/profile/views/profile_view.dart';
+import 'package:pendaftaran_app/modules/register/views/register_view.dart';
+import 'package:pendaftaran_app/modules/welcome/views/welcome_view.dart';
 class AppPages {
   static final pages = [
     GetPage(
+      name: AppRoutes.welcome,
+      page: () => const WelcomeView(),
+      binding: AuthBinding(),
+      ),
+    GetPage(
       name: AppRoutes.home,
       page: () => const HomeView(),
+      binding: AuthBinding(),
+      ),
+    GetPage(
+      name: AppRoutes.register,
+      page: () => const RegisterView(),
+      binding: AuthBinding(),
+      ),
+    GetPage(
+      name: AppRoutes.login,
+      page: () => const LoginView(),
+      binding: AuthBinding(),
+      ),
+    GetPage(
+      name: AppRoutes.profile,
+      page: () => const ProfileView(),
       binding: AuthBinding(),
       ),
   ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:pendaftaran_app/core/routes/routes/app_pages.dart';
+import 'package:pendaftaran_app/core/routes/routes/app_routes.dart';
+import 'package:pendaftaran_app/main_binding.dart';
 void main() {
   runApp(const Pendaftaran());
 }
 
-class Pendaftaran extends StatelessWidget {
+class Pendaftaran extends StatefulWidget {
   const Pendaftaran({super.key});
 
+  @override
+  State<Pendaftaran> createState() => _PendaftaranState();
+}
+
+class _PendaftaranState extends State<Pendaftaran> {
   // This widget is the root of your application.
   @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return GetMaterialApp(
+      title: 'Pendaftaran',
+      debugShowCheckedModeBanner: false,
+      initialBinding: MainBinding(),
+      initialRoute: AppRoutes.welcome,
+      getPages: AppPages.pages,
     );
   }
-}
   
+  void initialization() {
+    
+  }
+}
+
 // class MyHomePage extends StatefulWidget {
 //   const MyHomePage({super.key, required this.title});
 
